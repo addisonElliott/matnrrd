7b4a670bc8bc18856d6c56f383d3cee0be9c83d7
Addison Elliott <addison.elliott@gmail.com>
2018-03-27 16:14:15 -0500

    Create TODO list in README and update test function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # matnrrd
 MATLAB NRRD library that provides easy-to-use functions to load and save NRRD files.
+
+# TODO
+* Create nice, detailed documentation for nrrdread and nrrdwrite
+* Write tests for these functions to validate they are working
+* Check out and diagnose GZIP temp file issue
+* Get nrrdwrite working for ASCII, GZIP & regular
--- test.m
+++ test.m
@@ -1,13 +1,13 @@
-% filename = '303PREDIASTOLE.seg.nrrd';
-filename = 'an-hist.nrrd';
-% filename = 'EndDiastole.nrrd';
-% filename = 'SCAT.nrrd';
+filename = 'data/303PREDIASTOLE.seg.nrrd';
+% filename = 'data/an-hist.nrrd';
+% filename = 'data/EndDiastole.nrrd';
+% filename = 'data/SCAT.nrrd';
 
 [data, metadata] = nrrdread(filename);
 
-nrrdwrite('test.nrrd', data, metadata);
+% nrrdwrite('test.nrrd', data, metadata);
 
 % data = permute(data, [2 1 3]);
-% data = permute(data, [2 3 4 1]);
+data = permute(data, [2 3 4 1]);
 % 
-% imshow(data(:, :, 10, 1) * 255);
+imshow(data(:, :, 10, 1) * 255);
