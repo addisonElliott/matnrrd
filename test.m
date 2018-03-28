74aaacbff2c38dfb944dfb1ba1b79de03613a98f
Addison Elliott <addison.elliott@gmail.com>
2018-03-27 21:57:49 -0500

    Create test for 3D NRRD matrix

--- test.m
+++ test.m
@@ -11,7 +11,7 @@
 % assert(all(all(data == reshape(1:27, [3 9])')), 'Invalid data matrix for test2d');
 % assert(metadata.dimension == 2, 'Dimension is not 2 for test2d');
 % assert(strcmp(metadata.type, 'uint16'), 'Type is not uint16 for test2d');
-% assert(all(metadata.sizes == [3 9]), 'Vector length is not 27 for test2d');
+% assert(all(metadata.sizes == [3 9]), 'Sizes not right for test2d');
 % assert(strcmp(metadata.encoding, 'ascii'), 'Not ASCII encoding for test2d');
 % assert(metadata.spacedimension == 2, 'Space dimension is not 2 for test2d');
 % assert(all(metadata.spacings == [1.0458 2]), 'Spacing not correct for test2d');
@@ -21,9 +21,22 @@
 % assert(all(metadata.spaceorigin == [100 200]), 'Space origin not correct for test2d');
 
 % [data, metadata] = nrrdread('data/test3d.nrrd');
+% 
+% assert(all(all(all(data == permute(reshape(1:27, [3 3 3]), [3 2 1])))), 'Invalid data matrix for test3d');
+% assert(metadata.dimension == 3, 'Dimension is not 3 for test3d');
+% assert(strcmp(metadata.type, 'uint32'), 'Type is not uint32 for test3d');
+% assert(all(metadata.sizes == [3 3 3]), 'Sizes not right for test3d');
+% assert(strcmp(metadata.encoding, 'ascii'), 'Not ASCII encoding for test3d');
+% assert(strcmp(metadata.space, 'left-posterior-superior'), 'Space not right for test3d');
+% assert(all(all(metadata.spacedirections == [1 0 0; 0 1 0; 0 0 1])), 'Space directions not correct for test3d');
+% assert(all(strcmp(metadata.kinds, 'domain')), 'Kinds not correct for test3d');
+% assert(all(strcmp(metadata.spaceunits, 'mm')), 'Space units not correct for test3d');
+% assert(all(metadata.spaceorigin == [100.1 200.3 -500]), 'Space origin not correct for test3d');
 
 
 
+
+% TODO Create option to add quotation format for array of strings...
 
 % filename = 'data/303PREDIASTOLE.seg.nrrd';
 % filename = 'data/an-hist.nrrd';
