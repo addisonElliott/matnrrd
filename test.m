0d5743a2e3a7a8221db988c768decae3427d1143
Addison Elliott <addison.elliott@gmail.com>
2018-03-28 23:03:35 -0500

    Update tests so they pass

--- test.m
+++ test.m
@@ -1,6 +1,6 @@
 [data, metadata] = nrrdread('data/test1d_ascii.nrrd');
 
-assert(all(data == 1:27), 'Invalid data matrix for test1d');
+assert(all(data == (1:27)'), 'Invalid data matrix for test1d');
 assert(metadata.dimension == 1, 'Dimension is not 1 for test1d');
 assert(strcmp(metadata.type, 'uint8'), 'Type is not uint8 for test1d');
 assert(metadata.sizes == 27, 'Vector length is not 27 for test1d');
@@ -9,7 +9,7 @@ assert(strcmp(metadata.encoding, 'ascii'), 'Not ASCII encoding for test1d');
 
 [data, metadata] = nrrdread('data/test2d_ascii.nrrd');
 
-assert(all(all(data == reshape(1:27, [3 9])')), 'Invalid data matrix for test2d');
+assert(all(all(data == reshape(1:27, [3 9]))), 'Invalid data matrix for test2d');
 assert(metadata.dimension == 2, 'Dimension is not 2 for test2d');
 assert(strcmp(metadata.type, 'uint16'), 'Type is not uint16 for test2d');
 assert(all(metadata.sizes == [3 9]), 'Sizes not right for test2d');
@@ -24,7 +24,7 @@ assert(all(metadata.spaceorigin == [100 200]), 'Space origin not correct for tes
 
 [data, metadata] = nrrdread('data/test3d_ascii.nrrd');
 
-assert(all(all(all(data == permute(reshape(1:27, [3 3 3]), [3 2 1])))), 'Invalid data matrix for test3d');
+assert(all(all(all(data == reshape(1:27, [3 3 3])))), 'Invalid data matrix for test3d');
 assert(metadata.dimension == 3, 'Dimension is not 3 for test3d');
 assert(strcmp(metadata.type, 'uint32'), 'Type is not uint32 for test3d');
 assert(all(metadata.sizes == [3 3 3]), 'Sizes not right for test3d');
@@ -38,7 +38,7 @@ assert(all(metadata.spaceorigin == [100.1 200.3 -500]), 'Space origin not correc
 
 [data, metadata] = nrrdread('data/test1d_raw.nrrd');
 
-assert(all(data == 1:27), 'Invalid data matrix for test1d');
+assert(all(data == (1:27)'), 'Invalid data matrix for test1d');
 assert(metadata.dimension == 1, 'Dimension is not 1 for test1d');
 assert(strcmp(metadata.type, 'uint8'), 'Type is not uint8 for test1d');
 assert(metadata.sizes == 27, 'Vector length is not 27 for test1d');
@@ -47,7 +47,7 @@ assert(strcmp(metadata.encoding, 'raw'), 'Not raw encoding for test1d');
 
 [data, metadata] = nrrdread('data/test2d_raw.nrrd');
 
-assert(all(all(data == reshape(1:27, [3 9])')), 'Invalid data matrix for test2d');
+assert(all(all(data == reshape(1:27, [3 9]))), 'Invalid data matrix for test2d');
 assert(metadata.dimension == 2, 'Dimension is not 2 for test2d');
 assert(strcmp(metadata.type, 'uint16'), 'Type is not uint16 for test2d');
 assert(all(metadata.sizes == [3 9]), 'Sizes not right for test2d');
@@ -62,7 +62,7 @@ assert(all(metadata.spaceorigin == [100 200]), 'Space origin not correct for tes
 
 [data, metadata] = nrrdread('data/test3d_raw.nrrd');
 
-assert(all(all(all(data == permute(reshape(1:27, [3 3 3]), [3 2 1])))), 'Invalid data matrix for test3d');
+assert(all(all(all(data == reshape(1:27, [3 3 3])))), 'Invalid data matrix for test3d');
 assert(metadata.dimension == 3, 'Dimension is not 3 for test3d');
 assert(strcmp(metadata.type, 'uint32'), 'Type is not uint32 for test3d');
 assert(all(metadata.sizes == [3 3 3]), 'Sizes not right for test3d');
@@ -78,7 +78,7 @@ assert(all(metadata.spaceorigin == [100.1 200.3 -500]), 'Space origin not correc
 [data, metadata] = nrrdread('data/test3d_ascii.nrrd');
 metadata.endian = 'big';
 
-assert(all(all(all(data == permute(reshape(1:27, [3 3 3]), [3 2 1])))), 'Invalid data matrix for test3d');
+assert(all(all(all(data == reshape(1:27, [3 3 3])))), 'Invalid data matrix for test3d');
 assert(metadata.dimension == 3, 'Dimension is not 3 for test3d');
 assert(strcmp(metadata.type, 'uint32'), 'Type is not uint32 for test3d');
 assert(all(metadata.sizes == [3 3 3]), 'Sizes not right for test3d');
@@ -93,7 +93,7 @@ assert(all(metadata.spaceorigin == [100.1 200.3 -500]), 'Space origin not correc
 % Test big endian for raw encoding
 [data, metadata] = nrrdread('data/test3d_bigendian_raw.nrrd');
 
-assert(all(all(all(data == permute(reshape(1:27, [3 3 3]), [3 2 1])))), 'Invalid data matrix for test3d');
+assert(all(all(all(data == reshape(1:27, [3 3 3])))), 'Invalid data matrix for test3d');
 assert(metadata.dimension == 3, 'Dimension is not 3 for test3d');
 assert(strcmp(metadata.type, 'uint32'), 'Type is not uint32 for test3d');
 assert(all(metadata.sizes == [3 3 3]), 'Sizes not right for test3d');
@@ -110,7 +110,7 @@ assert(all(metadata.spaceorigin == [100.1 200.3 -500]), 'Space origin not correc
 % will be present. Need to force the endianness
 [data, metadata] = nrrdread('data/test3d_bigendian_raw_noendianfield.nrrd', 'Endian', 'big');
 
-assert(all(all(all(data == permute(reshape(1:27, [3 3 3]), [3 2 1])))), 'Invalid data matrix for test3d');
+assert(all(all(all(data == reshape(1:27, [3 3 3])))), 'Invalid data matrix for test3d');
 assert(metadata.dimension == 3, 'Dimension is not 3 for test3d');
 assert(strcmp(metadata.type, 'uint32'), 'Type is not uint32 for test3d');
 assert(all(metadata.sizes == [3 3 3]), 'Sizes not right for test3d');
@@ -125,7 +125,7 @@ assert(all(metadata.spaceorigin == [100.1 200.3 -500]), 'Space origin not correc
 
 [data, metadata] = nrrdread('data/test1d_gzip.nrrd');
 
-assert(all(data == 1:27), 'Invalid data matrix for test1d');
+assert(all(data == (1:27)'), 'Invalid data matrix for test1d');
 assert(metadata.dimension == 1, 'Dimension is not 1 for test1d');
 assert(strcmp(metadata.type, 'uint8'), 'Type is not uint8 for test1d');
 assert(metadata.sizes == 27, 'Vector length is not 27 for test1d');
@@ -134,7 +134,7 @@ assert(strcmp(metadata.encoding, 'gzip'), 'Not gzip encoding for test1d');
 
 [data, metadata] = nrrdread('data/test2d_gzip.nrrd');
 
-assert(all(all(data == reshape(1:27, [3 9])')), 'Invalid data matrix for test2d');
+assert(all(all(data == reshape(1:27, [3 9]))), 'Invalid data matrix for test2d');
 assert(metadata.dimension == 2, 'Dimension is not 2 for test2d');
 assert(strcmp(metadata.type, 'uint16'), 'Type is not uint16 for test2d');
 assert(all(metadata.sizes == [3 9]), 'Sizes not right for test2d');
@@ -149,7 +149,7 @@ assert(all(metadata.spaceorigin == [100 200]), 'Space origin not correct for tes
 
 [data, metadata] = nrrdread('data/test3d_gzip.nrrd');
 
-assert(all(all(all(data == permute(reshape(1:27, [3 3 3]), [3 2 1])))), 'Invalid data matrix for test3d');
+assert(all(all(all(data == reshape(1:27, [3 3 3])))), 'Invalid data matrix for test3d');
 assert(metadata.dimension == 3, 'Dimension is not 3 for test3d');
 assert(strcmp(metadata.type, 'uint32'), 'Type is not uint32 for test3d');
 assert(all(metadata.sizes == [3 3 3]), 'Sizes not right for test3d');
